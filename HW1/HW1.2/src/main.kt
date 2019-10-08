b586766753bba9f25d7c56a89c2ad2b99fd8b0a6
evertimes7@gmail.com <evertimes7@gmail.com>
2019-10-08 17:36:52 +0300

    fix

--- HW1/HW1.2/src/main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    println(lesser(21,4,1,22))
-}
-fun lesser(n1:Int,n2:Int,n3:Int,n4:Int):Int{
-    var g1 = if(n1<n2) n1 else n2
-    var g2 = if(n3<n4) n3 else n4
-    return if(g1<g2) g1 else g2
-}
