2a20b353522ddde7e2a8afeb443729ddc3e123e9
evertimes <evertimes7@gmail.com>
2019-11-03 21:28:27 +0300

    task 10.1 completed

---
+++ HW10.1/src/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val myList = listOf("Крош", "Ежик", "Нюша", "Бараш")
+    val mySet = setOf("Крош", "Ежик", "Нюша", "Бараш")
+    for (i in myList)
+        print("$i ")
+    println()
+    for (i in mySet)
+        print("$i ")
+}
