73a4f9b3bc43b696eb2ac0451737269e7c2d1f7e
evertimes <evertimes7@gmail.com>
2019-11-16 21:20:43 +0300

    XML release

--- HW14/src/XML.kt
+++ HW14/src/XML.kt
@@ -1,19 +1,22 @@
-open class TAG(val tagName:String){
+open class TAG(val tagName: String) {
     var children = mutableListOf<TAG>()
-    open var content:String? = null
-    fun <T: TAG> doInit(child:T,init: T.() -> Unit){
+    open var content: String? = null
+    fun <T : TAG> doInit(child: T, init: T.() -> Unit) {
         child.init()
         children.add(child)
     }
 }
-class OBJ : TAG("object"){
-    @Deprecated(level = DeprecationLevel.ERROR,message="no content allowed in this tag")
-    override var content:String? = null
-    fun name(init: NAME.() -> Unit) = doInit(NAME(),init)
-    fun surname(init: SURNAME.() -> Unit) = doInit(SURNAME(),init)
-    fun birthDate(init: BIRTHDATE.()->Unit) = doInit(BIRTHDATE(),init)
-    fun addresses(init: ADDRESSES.()->Unit) = doInit(ADDRESSES(),init)
+
+class OBJ : TAG("object") {
+    @Deprecated(level = DeprecationLevel.ERROR, message = "no content allowed in this tag")
+    override var content: String? = null
+
+    fun name(init: NAME.() -> Unit) = doInit(NAME(), init)
+    fun surname(init: SURNAME.() -> Unit) = doInit(SURNAME(), init)
+    fun birthDate(init: BIRTHDATE.() -> Unit) = doInit(BIRTHDATE(), init)
+    fun addresses(init: ADDRESSES.() -> Unit) = doInit(ADDRESSES(), init)
 }
+
 fun obj(init: OBJ.() -> Unit) = OBJ().apply(init)
 
 class NAME : TAG("name")
@@ -21,23 +24,24 @@ class SURNAME : TAG("surname")
 class BIRTHDATE : TAG("birthDate")
 class ADDRESS : TAG("address")
 
-class ADDRESSES : TAG("addresses"){
-    @Deprecated(level = DeprecationLevel.ERROR,message="no content allowed in this tag")
-    override var content:String? = null
-    fun address(init: ADDRESS.()->Unit) = doInit(ADDRESS(),init)
+class ADDRESSES : TAG("addresses") {
+    @Deprecated(level = DeprecationLevel.ERROR, message = "no content allowed in this tag")
+    override var content: String? = null
+
+    fun address(init: ADDRESS.() -> Unit) = doInit(ADDRESS(), init)
 }
 
-fun printXML(tag: TAG){
-    if(tag.children.size ==0 ){
+fun printXML(tag: TAG) {
+    if (tag.children.size == 0) {
         print("<${tag.tagName}>")
-        if(tag.content!=null) {
+        if (tag.content != null) {
             print(tag.content)
         }
-    }else{
+    } else {
         println("<${tag.tagName}>")
     }
-    if(tag.children.size !=0 ){
-        for(i in tag.children){
+    if (tag.children.size != 0) {
+        for (i in tag.children) {
             printXML(i)
         }
     }
--- HW14/src/main.kt
+++ HW14/src/main.kt
@@ -3,22 +3,22 @@ fun main() {
 }
 
 
-val myObject = obj{
-    name{
-        content="Andrey"
+val myObject = obj {
+    name {
+        content = "Andrey"
     }
-    surname{
-        content="Orlov"
+    surname {
+        content = "Orlov"
     }
-    birthDate{
-        content="15.06.2000"
+    birthDate {
+        content = "15.06.2000"
     }
     addresses {
-        address{
-            content="MyAddress"
+        address {
+            content = "MyAddress"
         }
         address {
-            content="MyAddress2"
+            content = "MyAddress2"
         }
     }
 }
