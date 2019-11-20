0408d18093df9c74fc8dbb05798da90467afc5e9
evertimes <evertimes7@gmail.com>
2019-11-20 14:57:37 +0300

    removed trash

--- HW8.1/src/main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    listOf("three", "two", "one").forEach {
-        if (it == "one") {
-            return@forEach
-        }
-        println(it)
-    }
-    println("boom!")
-}
--- HW8.3/src/main/kotlin/Pet.kt
+++
@@ -1,8 +0,0 @@
-import kotlinx.coroutines.delay
-
-class Pet(val nickname:String,private val mealDuration:Long) {
-    suspend fun eat(){
-        delay(mealDuration)
-        println("$nickname покушал")
-    }
-}
